f9f075fc79eefb14711e0e1c41d2bc28b15da66c
IceImo-P <imoya.ice@gmail.com>
2021-08-17 13:23:35 +0900

    Fix: Crashing TimePeriodPreferenceUtil.isInPeriod if preference is not set

--- lib/src/main/java/net/imoya/android/preference/controller/TimePeriodPreferenceUtil.kt
+++ lib/src/main/java/net/imoya/android/preference/controller/TimePeriodPreferenceUtil.kt
@@ -14,19 +14,19 @@ object TimePeriodPreferenceUtil {
      *
      * @param sharedPreferences [SharedPreferences]
      * @param key               設定値のキー
-     * @return 読み取った値が格納された [TimePeriod]
+     * @return 設定値が存在し読み取りに成功した場合は [TimePeriod], その他の場合は null
      */
     @JvmStatic
-    fun getTimePeriod(sharedPreferences: SharedPreferences, key: String): TimePeriod {
+    fun getTimePeriod(sharedPreferences: SharedPreferences, key: String): TimePeriod? {
         // Log.d(TAG, "getTimePeriod: start.");
         val stored = sharedPreferences.getString(key, null)
         return if (stored != null) {
             try {
                 parse(stored)
             } catch (ex: Exception) {
-                TimePeriod()
+                null
             }
-        } else TimePeriod()
+        } else null
     }
 
     /**
@@ -38,13 +38,13 @@ object TimePeriodPreferenceUtil {
      * @param key               Preference の Key を表す文字列。
      * @param hourOfDay         時刻の時(0-23)
      * @param minute            時刻の分(0-59)
-     * @return 指定の時刻が範囲に含まれている場合は true, それ以外の場合は false。
+     * @return 正常な設定値が存在し、且つ指定の時刻が範囲に含まれている場合は true, それ以外の場合は false
      */
     @JvmStatic
     fun isInPeriod(
         sharedPreferences: SharedPreferences, key: String, hourOfDay: Int, minute: Int
     ): Boolean {
         val value = getTimePeriod(sharedPreferences, key)
-        return value.isInPeriod(hourOfDay, minute, 0)
+        return value?.isInPeriod(hourOfDay, minute, 0) ?: false
     }
 }
