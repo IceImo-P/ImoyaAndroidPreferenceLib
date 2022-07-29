b81ebc3ea24fd771698818bd39b971a86be9fb59
IceImo-P <imoya.ice@gmail.com>
2022-07-30 02:02:27 +0900

    Release 2.0.1 - Add missing classes

--- lib/src/main/java/net/imoya/android/preference/controller/editor/NumberAndUnitDialogEditor.kt
+++ lib/src/main/java/net/imoya/android/preference/controller/editor/NumberAndUnitDialogEditor.kt
@@ -14,13 +14,10 @@
  * limitations under the License.
  */
 
-package net.imoya.android.preference.controller
+package net.imoya.android.preference.controller.editor
 
 import android.content.Intent
 import android.content.SharedPreferences
-import android.os.Parcel
-import android.os.Parcelable
-import android.os.Parcelable.Creator
 import android.text.InputType
 import androidx.annotation.CallSuper
 import net.imoya.android.dialog.DialogParent
@@ -28,6 +25,8 @@ import net.imoya.android.dialog.InputDialog
 import net.imoya.android.dialog.NumberInputDialog
 import net.imoya.android.preference.Constants
 import net.imoya.android.preference.PreferenceLog
+import net.imoya.android.preference.model.state.NumberAndUnitEditorState
+import net.imoya.android.preference.model.state.ScreenEditorState
 import net.imoya.android.preference.view.NumberAndUnitPreferenceView
 import net.imoya.android.preference.view.PreferenceView
 import net.imoya.android.preference.view.SingleValuePreferenceView
@@ -35,106 +34,26 @@ import net.imoya.android.preference.view.SingleValuePreferenceView
 /**
  * 単位表示付き整数設定値編集コントローラ
  *
- * [NumberAndUnitPreferenceView] と組み合わせて使用することを想定しています。
+ * * 編集画面はダイアログを使用します。
+ * * [NumberAndUnitPreferenceView] と組み合わせて使用することを想定しています。
  */
 @Suppress("unused")
-open class NumberAndUnitPreferenceEditor(
+open class NumberAndUnitDialogEditor(
     parent: DialogParent? = null,
     preferences: SharedPreferences? = null,
     requestCode: Int = Constants.DEFAULT_REQUEST_CODE
-) : DialogPreferenceEditor(parent, preferences, requestCode) {
-    /**
-     * 状態オブジェクト
-     */
-    protected open class State : PreferenceEditor.State {
-        /**
-         * デフォルト値
-         */
-        var defaultValue = 0
-
-        /**
-         * 設定可能な最小値
-         */
-        var minValue = Int.MIN_VALUE
-
-        /**
-         * 設定可能な最大値
-         */
-        var maxValue = Int.MAX_VALUE
-
-        /**
-         * 単位
-         */
-        var unit: String? = null
-
-        /**
-         * 入力欄のヒント文字列
-         */
-        var hint: String? = null
-
-        constructor() : super()
-
-        /**
-         * [Parcel] の内容で初期化するコンストラクタ
-         *
-         * @param parcel [Parcel]
-         */
-        protected constructor(parcel: Parcel) : super(parcel) {
-            defaultValue = parcel.readInt()
-            minValue = parcel.readInt()
-            maxValue = parcel.readInt()
-            unit = parcel.readString()
-            hint = parcel.readString()
-        }
-
-        override fun writeToParcel(dest: Parcel, flags: Int) {
-            super.writeToParcel(dest, flags)
-            dest.writeInt(defaultValue)
-            dest.writeInt(minValue)
-            dest.writeInt(maxValue)
-            dest.writeString(unit)
-            dest.writeString(hint)
-        }
-
-        companion object {
-            /**
-             * [Parcelable] 対応用 [Creator]
-             */
-            @JvmField
-            val CREATOR: Creator<State> = object : Creator<State> {
-                /**
-                 * [Parcel] の内容を保持するオブジェクトを生成して返します。
-                 *
-                 * @param parcel [Parcel]
-                 * @return [Parcel] の内容を保持するオブジェクト
-                 */
-                override fun createFromParcel(parcel: Parcel): State {
-                    return State(parcel)
-                }
-
-                /**
-                 * オブジェクトの配列を生成して返します。
-                 *
-                 * @param size 配列のサイズ
-                 * @return 配列
-                 */
-                override fun newArray(size: Int): Array<State?> {
-                    return arrayOfNulls(size)
-                }
-            }
-        }
-    }
+) : DialogEditor(parent, preferences, requestCode) {
 
     fun setHint(hint: String?) {
-        (state as State).hint = hint
+        (state as NumberAndUnitEditorState).hint = hint
     }
 
     override fun isCompatibleView(view: PreferenceView): Boolean {
         return view is NumberAndUnitPreferenceView
     }
 
-    override fun createState(): PreferenceEditor.State {
-        return State()
+    override fun createState(): ScreenEditorState {
+        return NumberAndUnitEditorState()
     }
 
     @CallSuper
@@ -144,7 +63,7 @@ open class NumberAndUnitPreferenceEditor(
         if (view !is NumberAndUnitPreferenceView) {
             throw IllegalArgumentException("View must be NumberAndUnitPreferenceView")
         }
-        val currentState = state as State
+        val currentState = state as NumberAndUnitEditorState
 
         PreferenceLog.v(TAG) {
             "setupState: defaultValue = ${view.defaultValue}, minValue = ${
@@ -165,7 +84,7 @@ open class NumberAndUnitPreferenceEditor(
             throw IllegalStateException("View must be SingleValuePreferenceView")
         }
         checkAndWarnRequestCode()
-        val currentState = state as State
+        val currentState = state as NumberAndUnitEditorState
 
         val inputType =
             if (currentState.minValue < 0) {
@@ -182,11 +101,12 @@ open class NumberAndUnitPreferenceEditor(
             .show()
     }
 
-    override fun saveInput(resultCode: Int, data: Intent) {
+    override fun saveInput(resultCode: Int, data: Intent?) {
+        if (data == null) throw IllegalArgumentException("data is null")
         val currentPreferences = checkPreferences()
         val key = checkKey()
         val value = data.getIntExtra(
-            InputDialog.EXTRA_KEY_INPUT_VALUE, (state as State).defaultValue
+            InputDialog.EXTRA_KEY_INPUT_VALUE, (state as NumberAndUnitEditorState).defaultValue
         )
         currentPreferences.edit().putInt(key, value).apply()
     }
